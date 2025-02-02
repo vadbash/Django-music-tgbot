08bfcdf85759742a4fe52098136f288c7ee4c9ab
vadbash <vadusikvadim@gmail.com>
2025-02-02 16:27:20 +0100

    add permissions

--- telegram/bot/views.py
+++ telegram/bot/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .models import ChatBotUser, MessageHistory
 from rest_framework import viewsets
 from .serializers import UserSerializer, MessageHistorySerializer
+from rest_framework import permissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = ChatBotUser.objects.all().order_by('-reg_date')
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated] 
 
 class MessageHistoryViewSet(viewsets.ModelViewSet):
     queryset = MessageHistory.objects.all()
     serializer_class = MessageHistorySerializer
+    permission_classes = [permissions.IsAuthenticated] 
